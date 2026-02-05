adefe3ec9006d82ccc1585596f419697dabf2f08
sasmithx <sasmithgit@gmail.com>
2026-02-05 21:37:22 +0530

    function updated

--- 019_functions/main.go
+++ 019_functions/main.go
@@ -8,7 +8,23 @@ func add(a int, b int) int {
 	return a + b
 }
 
+func SumAndProduct(a int, b int) (int, int) {
+	sum := a + b
+	product := a * b
+
+	return sum, product
+}
+
 func main() {
 	sum1 := add(3, 5)
-	fmt.Println("Sum1:", sum1)
+	s, p := SumAndProduct(4, 6)
+	fmt.Println("Sum1:", sum1, s, p)
+
+	// _
+
+	onlySum, _ := SumAndProduct(10, 2)
+
+
+	fmt.Println("Sum1:", sum1, s, p)
+	fmt.Println("Only Sum:", onlySum)
 }
